0e8f8402c4d2671aa93b5d9c1b6950b743f51ec0
ClaudiodoSenai <claudiorobertojunior5@gmail.com>
2023-08-01 10:09:40 -0300

    Atualizado controller exercicio 8

---
+++ app/Http/Controllers/ExercicioOitoController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ExercicioOitoController extends Controller
+{
+    public function Tabuada(Request $request)
+    {
+        $numeros = []; 
+
+        for ($i = 1; $i <= 10; $i++) {
+            $resultado=$i*$request->entrada;
+            $concatenado=$request->entrada . ' X ' . $i. ' = ' . $resultado;
+          array_push($numeros,$concatenado);
+        }
+
+        return json_encode([
+            'valores'=> $numeros
+        ]);
+    
+        
+    }
+}
